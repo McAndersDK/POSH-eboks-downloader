ed21ae03aa60d2b55ad77f2310b3725c7a88a1ed
unknown <aan@T440SW7OP81.nclan.netcompany.dk>
2015-12-29 09:22:01 +0100

    Bug fixes and made parameters to the script

--- HentDokumenter.ps1
+++ HentDokumenter.ps1
@@ -1,13 +1,17 @@
-﻿# User Settings:
+﻿param(
+[string]$cpr,
+[string]$password,
+[string]$activation,
+[string]$folder = 'c:\data\private\eboks',
+[switch]$onlyunread = $false,
+[switch]$all = $true
+)
+# User Settings:
 $config = @{
-    cpr = ''
-    password = ''
-    activation=''
+    cpr = $cpr
+    password = $password
+    activation=$activation
     }
-$folder = "c:\data\private\eboks"
-$onlyunread = $false
-$all = $true
-
 
 # static strings
 $LOGIN_URL = "https://m.e-boks.dk/logon.aspx"
@@ -51,40 +55,40 @@ $msgs = $inbox.Forms.Item("messages_form")
 $auth = $msgs.Fields.auth
 $fuid = $msgs.Fields.fuid
 if($all) {
-    $inbox = Invoke-WebRequest "$INBOX_URL`?fuid=$fuid&vad=1&vnod=1000" -WebSession $session
+    $inbox = Invoke-WebRequest "$INBOX_URL`?fuid=$fuid&vdfp=0&vad=1&vnod=1000" -WebSession $session
 }
 foreach ($msg in $inbox.ParsedHtml.forms.item("messages_form").getElementsByTagName("LI")) {
     if($onlyunread) {
         if ($msg.innerHTML -like '*item_unread*') {
-           $msgid = ($msg.getElementsByTagName("a")[0].href -split "'")[1]
-        $date = ($msg.getElementsByTagName("span") | where className -eq "recieved").innerText
-        $sender = ($msg.getElementsByTagName("span") | where className -eq "sender").innerText -replace "\",""
-        $subject = ($msg.getElementsByTagName("span") | where className -eq "title").innerText -replace "\",""
+            $msgid = ($msg.getElementsByTagName("a")[0].href -split "'")[1]
+            $date = ($msg.getElementsByTagName("span") | where className -eq "recieved").innerText
+            $sender = ($msg.getElementsByTagName("span") | where className -eq "sender").innerText -replace "\\",""
+            $subject = ($msg.getElementsByTagName("span") | where className -eq "title").innerText -replace "\\|:",""
 
-        write-host "[*] Getting documents for: $subject"
+            write-host "[*] Getting documents for: $subject"
 
-        $form = @{"target"= $msgid; "auth"= $auth; "fuid"= $fuid}
-        $document = Invoke-WebRequest $DOC_URL -WebSession $session -Body $form
+            $form = @{"target"= $msgid; "auth"= $auth; "fuid"= $fuid}
+            $document = Invoke-WebRequest $DOC_URL -WebSession $session -Body $form
 
-        foreach( $el in ($document.ParsedHtml.getElementsByTagName("form") | % {$_.getElementsByTagName("li") | % {$_.getElementsByTagName("a")} } )) {
+            foreach( $el in ($document.ParsedHtml.getElementsByTagName("form") | % {$_.getElementsByTagName("li") | % {$_.getElementsByTagName("a")} } )) {
         
-            $url = $el.href -replace "about:", ""
-            $info = ($el.getelementsbytagname("span") | where classname -eq "info").innertext
-            $title = $el.textContent.replace($info,"").trim() -replace "`\",""
-            $filetype = $info.split(",")[0].replace("(","").trim()
-            $filename = "$title.$filetype"
-            write-host "[*] Downloading attachment: $filename"
-            mkdir "$folder\$sender\$subject" -Force
-            Invoke-WebRequest "$DOCVIEW_URL$url" -WebSession $session -OutFile "$folder\$sender\$date $subject\$filename"
+                $url = $el.href -replace "about:", ""
+                $info = ($el.getelementsbytagname("span") | where classname -eq "info").innertext
+                $title = $el.textContent.replace($info,"").trim() -replace "\\|:",""
+                $filetype = $info.split(",")[0].replace("(","").trim()
+                $filename = "$title.$filetype"
+                write-host "[*] Downloading attachment: $filename to folder $folder\$sender\$date $subject"
+                mkdir "$folder\$sender\$date $subject" -Force
+                Invoke-WebRequest "$DOCVIEW_URL$url" -WebSession $session -OutFile "$folder\$sender\$date $subject\$filename"
 
-        }
+            }
         }
     }
     else {
         $msgid = ($msg.getElementsByTagName("a")[0].href -split "'")[1]
         $date = ($msg.getElementsByTagName("span") | where className -eq "recieved").innerText
-        $sender = ($msg.getElementsByTagName("span") | where className -eq "sender").innerText -replace "`\",""
-        $subject = ($msg.getElementsByTagName("span") | where className -eq "title").innerText -replace "`\",""
+        $sender = ($msg.getElementsByTagName("span") | where className -eq "sender").innerText -replace "\\",""
+        $subject = ($msg.getElementsByTagName("span") | where className -eq "title").innerText -replace "\\|:",""
 
         write-host "[*] Getting documents for: $subject"
 
@@ -95,12 +99,14 @@ foreach ($msg in $inbox.ParsedHtml.forms.item("messages_form").getElementsByTagN
         
             $url = $el.href -replace "about:", ""
             $info = ($el.getelementsbytagname("span") | where classname -eq "info").innertext
-            $title = $el.textContent.replace($info,"").trim() -replace "`\",""
+            $title = $el.textContent.replace($info,"").trim() -replace "\\|:",""
             $filetype = $info.split(",")[0].replace("(","").trim()
             $filename = "$title.$filetype"
-            write-host "[*] Downloading attachment: $filename"
-            mkdir "$folder\$sender\$subject" -Force
-            Invoke-WebRequest "$DOCVIEW_URL$url" -WebSession $session -OutFile "$folder\$sender\$date $subject\$filename"
+            if(!(Test-Path "$folder\$sender\$date $subject\$filename")) {
+                write-host "[*] Downloading attachment: $filename to folder $folder\$sender\$date $subject"
+                mkdir "$folder\$sender\$date $subject" -Force
+                Invoke-WebRequest "$DOCVIEW_URL$url" -WebSession $session -OutFile "$folder\$sender\$date $subject\$filename"
+            }
 
         }
     }
